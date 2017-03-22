c6212efad39948ba48e9deba765bebf97c3fc4d7
Maxence Maireaux <maxence@realstorm.fr>
2017-03-22 15:17:16 +0100

    Fix Fedora 25

--- v2/packer/provision.sh
+++ v2/packer/provision.sh
@@ -40,6 +40,16 @@ else
 
 fi
 
+elif [ -f /etc/redhat-release ] ; then
+    # Remove hardware specific settings from eth0
+    sed -i -e 's/^\(HWADDR\|UUID\|IPV6INIT\|NM_CONTROLLED\|MTU\).*//;/^$/d' \
+        /etc/sysconfig/network-scripts/ifcfg-eth0
+    # Remove all kernels except the current version
+    rpm -qa | grep ^kernel-[0-9].* | sort | grep -v $(uname -r) | \
+        xargs -r yum -y remove
+    yum -y clean all
+fi
+
 
 
 ### Clean
