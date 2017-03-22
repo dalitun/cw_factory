fbdc34ecc492d3cb0e1b6b91015b69d343c4425b
Maxence Maireaux <maxence@realstorm.fr>
2017-03-22 15:27:42 +0100

    Fix Fedora 25

--- v2/packer/provision.sh
+++ v2/packer/provision.sh
@@ -44,6 +44,8 @@ elif [ -f /etc/redhat-release ] ; then
     # Remove hardware specific settings from eth0
     sed -i -e 's/^\(HWADDR\|UUID\|IPV6INIT\|NM_CONTROLLED\|MTU\).*//;/^$/d' \
         /etc/sysconfig/network-scripts/ifcfg-eth0
+    # Remove udev persistent-net.rules
+    rm -rf /etc/udev/rules.d/70-persistent-net.rules
     # Remove all kernels except the current version
     rpm -qa | grep ^kernel-[0-9].* | sort | grep -v $(uname -r) | \
         xargs -r yum -y remove
