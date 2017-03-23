b313a4cac563765b789d5e677adab8f3aa499f04
Maxence Maireaux <maxence@realstorm.fr>
2017-03-23 14:45:37 +0100

    fix

--- v2/pytesting_os/cloudinit/test_cloudinit.py
+++ v2/pytesting_os/cloudinit/test_cloudinit.py
@@ -4,10 +4,10 @@ from cloudinit import test_resources
 global test_resources
 
 def test_cloudinit_package():
-    ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('emacs --version')
+    ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('vi --version')
     cmd_stdout = ssh_stdout.read()
-    package_installed_by_userdata_is_present = (cmd_stdout.find('GNU Emacs') != -1)
-    print "Expecting to find 'GNU Emacs' in:\n" + cmd_stdout
+    package_installed_by_userdata_is_present = (cmd_stdout.find('vi') != -1)
+    print "Expecting to find 'Vi' in:\n" + cmd_stdout
 
     assert package_installed_by_userdata_is_present
 
