edd3963e51252ae4f7d3172895c8fda76fbcfaa1
Maxence Maireaux <maxence@realstorm.fr>
2017-03-24 13:16:20 +0100

    push

--- v2/pytesting_os/cloudinit/test_cloudinit.py
+++ v2/pytesting_os/cloudinit/test_cloudinit.py
@@ -7,14 +7,14 @@ def test_cloudinit_package():
     ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('vi --version')
     cmd_stdout = ssh_stdout.read()
     package_installed_by_userdata_is_present = (cmd_stdout.find('vi') != -1)
-    print "Expecting to find 'Vi' in:\n" + cmd_stdout
+    print "Vi IMproved" + cmd_stdout
 
     assert package_installed_by_userdata_is_present
 
 
 def test_cloudinit_runcmd():
-    ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('sudo ls /root')
+    ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('sudo ls /etc')
     cmd_stdout= ssh_stdout.read()
-    file_created_by_userdata_is_present = (cmd_stdout.find('cloud-init.txt') !=-1)
+    file_created_by_userdata_is_present = (cmd_stdout.find('hosts') !=-1)
     assert file_created_by_userdata_is_present
 
