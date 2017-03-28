0d0ee76a9cb27390d99dceca220329594934a955
Maxence Maireaux <maxence@realstorm.fr>
2017-03-28 11:50:01 +0200

    up

--- v2/02_packer.sh
+++ v2/02_packer.sh
@@ -7,8 +7,8 @@ while true
    heat stack-list | grep factory3 | cut -d "|" -f4 | grep "CREATE_COMPLETE"
    if  [ $? -eq 0 ]
       then
-        export NET_ID=$(heat output-show factory Network_id | sed -e 's/^"//' -e 's/"$//')
-        export SG_ID=$(heat output-show factory Security_group | sed -e 's/^"//'  -e 's/"$//')
+        export NET_ID=$(heat output-show factory3 Network_id | sed -e 's/^"//' -e 's/"$//')
+        export SG_ID=$(heat output-show factory3 Security_group | sed -e 's/^"//'  -e 's/"$//')
         break
       else
         echo "Wait for factory_network stack will be up"
