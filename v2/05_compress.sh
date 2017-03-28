8dae3e45c70eb2ae83a68cef9e6433310e884b3b
Maxence Maireaux <maxence@realstorm.fr>
2017-03-28 15:34:25 +0200

    up

--- v2/05_compress.sh
+++ v2/05_compress.sh
@@ -7,7 +7,7 @@ IMG_NAME="${OS_NAME}-${OS_VERSION}_${DATE}"
 
 glance image-download --file ./current.qcow2 ${IMG_ID}
 
-virt-sparsify --compress --tmp  "current.qcow2" "current-c.qcow2"  || exit 1
+virt-sparsify -v --compress --tmp  "current.qcow2" "current-c.qcow2"  || exit 1
 
 rm -rf current.qcow2
 
